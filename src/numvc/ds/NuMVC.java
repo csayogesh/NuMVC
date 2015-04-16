70132ba7c22cbf813615ed2c0146e075d0ed8604
yogesh <ybhosale12@gmail.com>
2015-04-16 21:58:55 +0530

    Some small change.

--- src/numvc/ds/NuMVC.java
+++ src/numvc/ds/NuMVC.java
@@ -6,16 +6,12 @@ import numvc.utilities.Converter;
 
 public class NuMVC {
 	public static void main(String[] args) throws IOException {
-		// int ip[][] = { {2, 6 }, { 1, 6, 3 }, { 2, 4, 5 }, { 3 }, {3, 6}, {1,
-		// 2, 5} };
+		@SuppressWarnings("unused")
+		int ip[][] = { { 2, 6 }, { 1, 6, 3 }, { 2, 4, 5 }, { 3 }, { 3, 6 },
+				{ 1, 2, 5 } };
 		Graph graph = new Graph(
-				Converter.convertEdgeList("src/benchworks/bhoslib/frb30-15-1.mis"));
-		graph.addToC(2);
-		graph.addToC(4);
-		graph.addToC(1);
-		graph.addToC(3);
-		graph.addToC(5);
-		graph.addToC(6);
+				Converter
+						.convertEdgeList("src/benchworks/bhoslib/frb30-15-1.mis"));
 		graph.computeNuMVC();
 		System.out.println(graph);
 	}
